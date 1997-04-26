7a585096eb8dcc8cc29dbc28e4fe3ea193cb51eb
James Youngman <jay@gnu.org>
1997-04-26 15:37:36 +0000

    Extract our mktime implementation into mktime.cc

--- sccsdate.cc
+++ sccsdate.cc
@@ -21,62 +21,6 @@
 static const char sccs_id[] = "@(#) MySC sccsdate.c 1.1 93/11/09 17:17:58";
 #endif
 
-#ifndef HAVE_MKTIME
-
-#define mktime LIDENT(mktime)
-
-#ifdef CONFIG_DECLARE_TIMEZONE
-extern long timezone;
-#endif
-#ifdef CONFIG_DECLARE_TZSET
-extern "C" void CDECL tzset();
-#endif
-
-/* This is a "good enough for MySC" implementation of mktime. */
-
-static time_t
-mktime(struct tm const *tm) {
-	const long day_secs = 24L * 60L * 60L;
-	int year = tm->tm_year - 70;
-	time_t t = (long) year * 365 * day_secs
-		   + (long) ((year + 1) / 4) * day_secs;
-	static long month_offs[12] = {
-		0L,
-		31L * day_secs,
-		59L * day_secs,
-		90L * day_secs,
-		120L * day_secs,
-		151L * day_secs,
-		181L * day_secs,
-		212L * day_secs,
-		243L * day_secs,
-		273L * day_secs,
-		304L * day_secs,
-		334L * day_secs
-	};
-
-	t += month_offs[tm->tm_mon] + (long) (tm->tm_mday - 1) * day_secs;
-	if (tm->tm_mon > 2 && tm->tm_year % 4 == 0) {
-		t += day_secs;
-        }
-
-	t += tm->tm_sec + tm->tm_min * 60 + (long) tm->tm_hour * 60 * 60;
-
-#ifdef HAVE_TIMEZONE
-	tzset();
-	t += timezone;
-#endif
-
-	if (localtime(&t)->tm_isdst > 0) {
-		t -= 60 * 60;
-	}
-
-	return t;
-}	       
-	
-#endif /* HAVE_MKTIME */   
-
-
 static int
 days_in_month(int mon, int year) {
 	switch(mon) {
