31c23526791725926bb7ba62fcdd715bb27c4b85
James Youngman <jay@gnu.org>
1997-05-10 12:09:57 +0000

    Remodelled the tests for individual manageability and rewrote them so standard SCCS passed them.   Not testes with CSSC yet.

--- tests/get/create.sh
+++ tests/get/create.sh
@@ -19,115 +19,36 @@ done
 remove base test/[xz].*
 remove test/[spx].passwd
 
+
 #
 # Create an SCCS file with several branches to work on.
+# We generally ignore stderr output since we produce "Warning: no id keywords"
+# more often than "real" SCCS.
 #
-(
-${admin} -itest/passwd.1 test/s.passwd
-${get} -e -g test/s.passwd
+docommand L1 "${admin} -itest/passwd.1 test/s.passwd" 0 "" IGNORE
+docommand L2 "${get} -e -g test/s.passwd"             0 "1.1\nnew delta 1.2\n" ""
 cp test/passwd.2 passwd
-${delta} -Y test/s.passwd
-${get} -e -g test/s.passwd
+docommand L3 "${delta} -y'' test/s.passwd" 0 "1.2\n1 inserted\n1 deleted\n1 unchanged\n" IGNORE
+docommand L4 "${get} -e -g test/s.passwd"  0 "1.2\nnew delta 1.3\n" ""
 cp test/passwd.3 passwd
-${delta} -Y test/s.passwd
-${get} -e -g -x1.2 test/s.passwd
+docommand L5 "${delta} -y'' test/s.passwd" 0 "1.3\n1 inserted\n1 deleted\n1 unchanged\n" IGNORE
+docommand L6 "${get} -e -g -x1.2 test/s.passwd" 0 "Excluded:\n1.2\n1.3\nnew delta 1.4\n" ""
 cp test/passwd.4 passwd
-${delta} -Y test/s.passwd
-${get} -e -g test/s.passwd
+docommand L7 "${delta} -y'' test/s.passwd" 0 "1.4\n1 inserted\n2 deleted\n1 unchanged\n" IGNORE
+docommand L8 "${get} -e -g test/s.passwd" 0 "1.4\nnew delta 1.5\n" ""
 cp test/passwd.5 passwd
-${delta} -Y test/s.passwd
-${get} -e -g -r1.3 test/s.passwd
+docommand L9 "${delta} -y'' test/s.passwd" 0 "1.5\n1 inserted\n1 deleted\n1 unchanged\n" IGNORE
+docommand L10 "${get} -e -g -r1.3 test/s.passwd" 0 "1.3\nnew delta 1.3.1.1\n" ""
 cp test/passwd.6 passwd
-${delta} -Y test/s.passwd
-${get} -r1.1 -p test/s.passwd > test/passwd.m1
-${DIFF} test/passwd.1 test/passwd.m1
-${get} -r1.2 -p test/s.passwd > test/passwd.m2
-${DIFF} test/passwd.2 test/passwd.m2
-${get} -r1.3 -p test/s.passwd > test/passwd.m3
-${DIFF} test/passwd.3 test/passwd.m3
-${get} -r1.4 -p test/s.passwd > test/passwd.m4
-${DIFF} test/passwd.4 test/passwd.m4
-${get} -r1.5 -p test/s.passwd > test/passwd.m5
-${DIFF} test/passwd.5 test/passwd.m5
-${get} -r1.3.1.1 -p test/s.passwd > test/passwd.m6
-${DIFF} test/passwd.6 test/passwd.m6
-) > log.stdout 2>log.stderr
+docommand L11 "${delta} -y'' test/s.passwd" 0 \
+    "1.3.1.1\n1 inserted\n1 deleted\n1 unchanged\n" IGNORE
 
-remove expected
-
-cat >expected <<EOF
-
-test/s.passwd
-1.1
-new delta 1.2
-
-test/s.passwd
-1.2
-new delta 1.3
-
-test/s.passwd
-Excluded: 1.2
-1.3
-new delta 1.4
-
-test/s.passwd
-1.4
-new delta 1.5
-
-test/s.passwd
-1.3
-new delta 1.3.1.1
-EOF
-
-diff expected log.stdout || fail output format error
-remove expected
-
-cat >expected <<EOF
-test/s.passwd: Warning: No id keywords.
-test/s.passwd: Warning: No id keywords.
-passwd: Warning: No id keywords.
-test/s.passwd: Warning: No id keywords.
-passwd: Warning: No id keywords.
-test/s.passwd: Warning: No id keywords.
-passwd: Warning: No id keywords.
-test/s.passwd: Warning: No id keywords.
-passwd: Warning: No id keywords.
-test/s.passwd: Warning: No id keywords.
-passwd: Warning: No id keywords.
-test/s.passwd: Warning: No id keywords.
-
-test/s.passwd
-1.1
-2 lines
-test/s.passwd: Warning: No id keywords.
-
-test/s.passwd
-1.2
-2 lines
-test/s.passwd: Warning: No id keywords.
-
-test/s.passwd
-1.3
-2 lines
-test/s.passwd: Warning: No id keywords.
-
-test/s.passwd
-1.4
-2 lines
-test/s.passwd: Warning: No id keywords.
-
-test/s.passwd
-1.5
-2 lines
-test/s.passwd: Warning: No id keywords.
-
-test/s.passwd
-1.3.1.1
-2 lines
-EOF
-
-diff expected log.stderr || fail stderr output format error.
-
-remove test expected log log.stdout log.stderr 
+do_output L12 "${get} -r1.1 -p test/s.passwd"      0 test/passwd.1 IGNORE
+do_output L13 "${get} -r1.2 -p test/s.passwd"      0 test/passwd.2 IGNORE
+do_output L14 "${get} -r1.3 -p test/s.passwd"      0 test/passwd.3 IGNORE
+do_output L15 "${get} -r1.4 -p test/s.passwd"      0 test/passwd.4 IGNORE
+do_output L16 "${get} -r1.5 -p test/s.passwd"      0 test/passwd.5 IGNORE
+do_output L17 "${get} -r1.3.1.1 -p test/s.passwd"  0 test/passwd.6 IGNORE
 
 success
+
