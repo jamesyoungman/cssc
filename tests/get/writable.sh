fec547875a4401d69944ae35148d9c5bbadf7542
James Youngman <jay@gnu.org>
1997-05-10 12:51:58 +0000

    Used the new, more consise macros; now works with both real SCCS and also CSSC.

--- tests/get/writable.sh
+++ tests/get/writable.sh
@@ -4,22 +4,31 @@
 # Import common functions & definitions.
 . test-common
 
+remove command.log log log.stdout log.stderr
+
 f=wrtest
+gfile=_g.$f
 remove s.$f
+
+# Generate empty file.
 : > $f
-$admin -n -i $f s.$f >/dev/null 2>&1 || fail admin failed.
+
+# Create the s. file and make sure it exists.
+docommand W1 "$admin -n -i$f s.$f" 0 "" IGNORE
+
 test -r s.$f         || fail admin did not create s.$f
 remove $f
-
 echo foo > $f
 chmod +w $f
 
-# The get should fail.
-$get s.$f >/dev/null 2>&1 && fail get returned success when gfile was writable
-gfile=_g.$f
-remove _g.$f
+# Try running get when gfile was writable -- it should fail.
+docommand W2 "$get s.$f" 1 IGNORE IGNORE
+remove $gfile
 test -e $gfile	    && miscarry could not remove _g.$f
-eval "$get -G$gfile s.$f" >/dev/null 2>&1|| fail Could not get s.$f into $gfile.
+
+# Now run get with the -G option and it should work even
+# though the file's usual name is occupied by a writable file.
+docommand W3 "$get -G$gfile s.$f" 0 "1.1\n0 lines\n" IGNORE
 remove $f s.$f $gfile
 
 success
