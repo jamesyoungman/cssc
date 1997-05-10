3e5a7c9d10f58fd933d2f78c31adbde14ac2e78b
James Youngman <jay@gnu.org>
1997-05-10 14:48:49 +0000

    Modified the version strings so that they show up with RCS ident and SCCS/CSSC's what, and also are human-readable.

--- version.cc
+++ version.cc
@@ -1,2 +1,7 @@
-extern const char mysc_version[] = "@(#) MySC Release: 2; Patchlevel: 0\n";
+#define version_core "GNU CSSC " VERSION "; Patchlevel: " PATCHLEVEL
+
+extern const char cssc_version_rcs[] = "$Version: " version_core " $";
+extern const char cssc_version_sccs[] = "@(#) " version_core;
+extern const char cssc_version[] = version_core;
+
 
