0b7a979d70fe874cb00fbaa178e9e9ffe9a03ce8
James Youngman <jay@gnu.org>
1997-05-21 00:01:28 +0000

    Invented the '!' modifier, similar to ':', but allowing empty options.  Note that this is to support empty -m and -y options for admin and delta, that this behaviour is for compatibility with "real" SCCS, and is unlike traditional or POSIX option processing.

--- getopt.cc
+++ getopt.cc
@@ -1,8 +1,9 @@
 /*
- * getopt.c
+ * getopt.cc
  *
- * By Ross Ridge
- * Public Domain
+ * From CSSC, Copyright (C) 1997, Free Software Foundation, Inc.
+ *
+ * Based on MySC, by Ross Ridge, which was placed in the Public Domain.
  * 
  * Members of the class getopt.
  *
@@ -16,57 +17,109 @@
 #include "getopt.h"
 
 #ifdef CONFIG_SCCS_IDS
-static const char rcs_id[] = "CSSC $Id: getopt.cc,v 1.3 1997/05/10 14:49:49 james Exp $";
+static const char rcs_id[] = "CSSC $Id: getopt.cc,v 1.4 1997/05/21 00:01:28 james Exp $";
 #endif
 
 int
-getopt::next() {
-	if (cindex == NULL || *cindex == '\0') {
-		if (index >= argc) {
-			return END_OF_ARGUMENTS;
-		}
-		cindex = argv[index];
-		if (cindex[0] != '-' || cindex[1] == '\0') {
-			return END_OF_ARGUMENTS;
-		}
-		index++;
-		if (cindex[1] == '-' && cindex[2] == '\0') {
-			return END_OF_ARGUMENTS;
-		}
-		cindex++;
+getopt::next()
+{
+  if (cindex == NULL || *cindex == '\0')
+    {
+      if (index >= argc)
+	{
+	  return END_OF_ARGUMENTS;
 	}
+      cindex = argv[index];
 
-	char c = *cindex++;
-	char *match = strchr(opts, c);
+      // A dash on its own is not a valid option so we have
+      // reached the end of the arguments.
+      if (cindex[0] != '-' || cindex[1] == '\0')
+	{
+	  return END_OF_ARGUMENTS;
+	}
+      index++;
+
+      // The ragument "--" is the POSIX signal for end-of-args.
+      if (cindex[1] == '-' && cindex[2] == '\0')
+	{
+	  return END_OF_ARGUMENTS;
+	}
+      cindex++;
+    }
+  // Collect the argument character.
+  char c = *cindex++;
+
+  // Look for the argument character in the option list.
+  char *match = strchr(opts, c);
 	
-	if (c == '\0' || match == NULL) {
-		if (opterr) {
-			quit(-2, "Unrecognized option '%c'.\n", c);
-		}
-		arg = cindex - 1;
-		return UNRECOGNIZED_OPTION;
+  if (c == '\0' || match == NULL)
+    {
+      if (opterr)		// set opterr for verbose error returns.
+	{
+	  quit(-2, "Unrecognized option '%c'.\n", c);
 	}
+      arg = cindex - 1;
+      return UNRECOGNIZED_OPTION;
+    }
 
-	if (match[1] == ':') {
-		if (*cindex == '\0') {
-			if (index >= argc) {
-				if (opterr) {
-					quit(-2, "Option '%c' requires"
-						 "an argument.\n", c);
-				}
-				arg = cindex - 1;
-				return MISSING_ARGUMENT;
-			}
-			arg = argv[index++];
-		} else {
-			arg = cindex;
+  // match[1] may contain either a modifier for an option,
+  // or the next option letter in the option list.
+
+  // If an option letter is followed by "!", this indicates
+  // that any argument must follow the option letter immediately.
+  int takes_arg;
+  int arg_catenated;
+
+  if (match[1] == ':')		// option takes an argument.
+    {
+      takes_arg = 1;
+      arg_catenated = 0;
+    }
+  else if (match[1] == '!')	// option takes a concatenated argument.
+    {
+      takes_arg = 1;
+      arg_catenated = 1;
+    }
+  else				// no argument.
+    {
+      takes_arg = arg_catenated = 0;
+    }
+  
+  
+  if (takes_arg)
+    {
+      if (*cindex == '\0' && (!arg_catenated) )
+	{
+	  // Option is of the form "-X", the argument is next
+	  // in the list. 
+	  if (index >= argc)
+	    {
+	      if (opterr)
+		{
+		  quit(-2, "Option '%c' requires"
+		       "an argument.\n", c);
 		}
-		cindex = NULL;
-	} else {
-		arg = NULL;
+	      arg = cindex - 1;
+	      return MISSING_ARGUMENT;
+	    }
+	  arg = argv[index++];
 	}
+      else
+	{
+	  // Option is of the form "-Xmumble", the argument
+	  // is "mumble".  Here, *cindex could be '\0', meaning
+	  // that the option is a "catenated arg" option like 
+	  // sccs-get's -y option, and there is no actual argument.
+	  arg = cindex;
+	}
+      cindex = NULL;
+    }
+  else				
+    {
+      arg = NULL;		// no argument taken by this option.
+    }
 
-	return c;
+  return c;
 }
 
 /* Local variables: */
