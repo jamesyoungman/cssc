3b03628052a0aa9c309309432cb8ed52559ee48b
James Youngman <jay@gnu.org>
1997-05-22 23:10:50 +0000

    Changed the test labels to be I* not T*.

--- tests/admin/i-option.sh
+++ tests/admin/i-option.sh
@@ -11,14 +11,14 @@ remove foo
 echo '%M%' > foo
 test `cat foo` = '%M%' || miscarry cannot create file foo.
 
-docommand T1 "${admin} -ifoo s.bar" 0 "" IGNORE
-docommand T2 "${get} -r1.1 -p s.bar"      0 "bar\n" IGNORE
+docommand I1 "${admin} -ifoo s.bar" 0 "" IGNORE
+docommand I2 "${get} -r1.1 -p s.bar"      0 "bar\n" IGNORE
 remove foo s.bar
 
 # -i on its own means read from stdin.
 echo baz | \
-docommand T3 "${admin} -i s.bar" 0 "" IGNORE
-docommand T4 "${get} -r1.1 -p s.bar"      0 "baz\n" IGNORE
+docommand I3 "${admin} -i s.bar" 0 "" IGNORE
+docommand I4 "${get} -r1.1 -p s.bar"      0 "baz\n" IGNORE
 
 remove s.bar foo bar command.log 
 
