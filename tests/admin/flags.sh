3c75ad1e91fa248690d9535ebad7e8d1e58abe85
James Youngman <jay@gnu.org>
1997-06-27 18:41:43 +0000

    Check null deltas too.

--- tests/admin/flags.sh
+++ tests/admin/flags.sh
@@ -8,7 +8,7 @@
 g=new.txt
 s=s.$g
 p=p.$g
-remove foo $s $g $p
+remove foo $s $g $p [zx].$g
 
 ###
 ### Tests for the 'v' flag; see also init-mrs.sh.
@@ -82,17 +82,26 @@ docommand b8 "${admin} -fn $s" 0 "" ""
 
 # Skip a release (2)
 docommand b9 "${get} -e -r3 $s" 0 "1.1\nnew delta 3.1\n1 lines\n" \
-	IGNORE
+   	    IGNORE
 echo "hello" >> $g
 docommand b10 "${delta} -y $s" 0 "3.1\n1 inserted\n0 deleted\n1 unchanged\n" IGNORE
+   
+# Check that a null delta was made for release 2, at all.
+docommand b11 "${prs} -r2.1 -d:I: $s" 0 "2.1\n" IGNORE
 
-
+# Check some details about that release.  The comment is 
+# "AUTO NULL DELTA", no deltas were included or excluded;
+# one delta was ignored; the predecessor sequence number must be 1; 
+# the sequence number of this delta must be 2, and the type must be 'D',
+# that is, a normal delta.
+docommand b12 "${prs} -r2.1 '-d:C:|:DI:|:DP:|:DS:|:DT:' $s" 0 \
+  'AUTO NULL DELTA\n||1|2|D\n' IGNORE
 
 ###
 ### Cleanup and exit.
 ###
 rm -rf test 
-remove foo $s $g $p command.log
+remove foo $s $g $p [zx].$g command.log
 
 success
 
