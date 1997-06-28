3d47210dad607ec924f15793161f48f634198e98
James Youngman <jay@gnu.org>
1997-06-28 12:09:20 +0000

    First attempt at allowing us to run the tests in a separate directory.

--- tests/all-tests.sh
+++ tests/all-tests.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+if [ $# -gt 0 ]
+then
+    srcdir=$1
+    shift
+else
+    srcdir=.
+fi
+echo Using $srcdir as the srcdir.
+
 set -e
 
 for i in admin delta get prs unget 
@@ -7,12 +16,12 @@ do
     echo ============== Tests for $i ==================
     ret=`pwd`
     cd $i
-
-    for s in *.sh
+    for s in ../${srcdir}/${i}/*.sh
     do
-	echo ------------ $i/$s ---------------------
+	testname=${i}/`basename $s`
+	echo ------------ $testname ---------------------
 	sh $s
-	echo ------------ pass: $i/$s ---------------
+	echo ------------ pass: $testname ---------------
     done
 
     cd $ret
