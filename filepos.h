ce36f62aa3604780fe8077f407c50aef4b73e2b6
James Youngman <jay@gnu.org>
1997-07-02 06:43:49 +0000

    Initial revision

---
+++ filepos.h
@@ -0,0 +1,71 @@
+// -*- C++ -*- Class for saving and restoring our file position.
+
+// We use fgetpos()/fsetpos() if available, else ftell()/fseek().
+// On systems where the two methods are distinct, both will surely
+// be provided?
+
+
+
+#include "cssc.h"
+
+#ifdef HAVE_FSETPOS
+
+class FilePosSaver		// with fsetpos()...
+{
+public:  
+  FilePosSaver(FILE*);		// save the position on this FILE...
+  ~FilePosSaver();		// and restore it in the destructor.
+private:
+
+  fpos_t pos;
+  FILE *f;
+};
+
+FilePosSaver::FilePosSaver(FILE *fp) : f(fp)
+{
+  if (0 != fgetpos(f, &pos))
+    quit(errno, "fgetpos() failed!"); // better, later; throw exception.
+}
+
+FilePosSaver::~FilePosSaver()
+{
+  if (0 != fsetpos(f, &pos))
+    quit(errno, "fsetpos() failed!");
+}
+
+#else
+
+class FilePosSaver		// no fsetpos()...
+{
+public:  
+  FilePosSaver(FILE*);		// save the position on this FILE...
+  ~FilePosSaver();		// and restore it in the destructor.
+private:
+
+  long   offset;
+  FILE   *f;
+};
+
+
+FilePosSaver::FilePosSaver(FILE *fp) : f(fp)
+{
+  offset = ftell(f);
+  if (offset == -1L)
+    quit(errno, "ftell() failed.");
+}
+
+FilePosSaver::~FilePosSaver()
+{
+  if (fseek(f, offset, 0) != 0)
+    quit(errno, "fseek() failed!");
+}
+
+
+
+
+
+
+
+
+#endif
+
