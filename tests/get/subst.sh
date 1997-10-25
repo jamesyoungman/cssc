155c3bd0b4a9d6d575306d3c2a31a9b86a30da83
James Youngman <jay@gnu.org>
1997-10-25 15:31:31 +0000

    All tests pass with "real" SCCS.

--- tests/get/subst.sh
+++ tests/get/subst.sh
@@ -9,36 +9,52 @@
 
 # Get a test file...
 s=s.keywords.txt
+output=get.output
+
+
 remove $s
 uudecode < keywords.uue || miscarry could not extract test file.
 
+# Expand all the keywords from the s.file and save the format in 
+# a temporary file.   We then examine this file later.
+echo_nonl "Preparing..."
+remove ${output}
+if ${get} -p $s > ${output} 2>/dev/null
+then
+    echo passed
+else
+    fail "$0: preparation step: could not run get -p."
+fi
+
+# Ckeck that the format of stderr is correct.
+docommand "stderr format" "${get} -p $s" 0 "IGNORE" "1.1\n83 lines\n"
+
 expands_to () {
-    # $1 -- label
-    # $2 -- format
-    # $3 -- expansion
-    # $4 -- stderr
-docommand $1 "${get} -p $s | grep \"^_${2}_ \" " 0 "$3" "$stderr"
+    # $1 -- format
+    # $2 -- expansion
+docommand "%${1}%" "egrep \"^_${1}_ \" <${output}" 0 "$2" ""
 }
 
-stderr="1.1\n83 lines\n"
-
-expands_to A A "_A_ @(#) keywords.txt 1.1@(#)\n" 
-expands_to B B "_B_ 0\n"                         
-expands_to C C "_C_ 16\n_C_ 17\n"                
-expands_to E E "_E_ 97/10/25\n"                  
-expands_to F F "_F_ s.keywords.txt\n"            
-expands_to G G "_G_ 10/25/97\n"                  
-expands_to I I "_I_ 1.1.0.0\n"                   
-expands_to L L "_L_ 1\n"                         
-expands_to M M "_M_ keywords.txt\n"	         
-expands_to P P "_P_ ${PWD}/s.keywords.txt\n"     
-expands_to Q Q "_Q_ \n"                          
-expands_to R R "_R_ 1\n"                         
-expands_to S S "_S_ 0\n"                         
-expands_to U U "_U_ 13:06:02\n"                  
-expands_to W W "_W_ @(#)keywords.txt	1.1"     
-expands_to Y Y "_Y_ \n"                          
-expands_to Z Z "_Z_ @(#)\n"                      
+# Examine each of the things formatted into the file and
+# check them against our expectations.
+#
+expands_to A "_A_ @(#) keywords.txt 1.1@(#)\n" 
+expands_to B "_B_ 0\n"                         
+expands_to C "_C_ 16\n_C_ 17\n"                
+expands_to E "_E_ 97/10/25\n"                  
+expands_to F "_F_ s.keywords.txt\n"            
+expands_to G "_G_ 10/25/97\n"                  
+expands_to I "_I_ 1.1\n" 
+expands_to L "_L_ 1\n"                         
+expands_to M "_M_ keywords.txt\n"	         
+expands_to P "_P_ ${PWD}/s.keywords.txt\n"     
+expands_to Q "_Q_ \n"                          
+expands_to R "_R_ 1\n"                         
+expands_to S "_S_ 0\n"                         
+expands_to U "_U_ 13:19:31\n"                  
+expands_to W "_W_ @(#)keywords.txt\t1.1\n"     
+expands_to Y "_Y_ \n"                          
+expands_to Z "_Z_ @(#)\n"                      
 
 # TODO: better tests for Q, D, H, T, Y
 
