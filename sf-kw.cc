3f0da0e7962a05ea5974c5973923bd8fadad44d8
James Youngman <jay@gnu.org>
1997-10-26 19:48:06 +0000

    Initial revision

---
+++ sf-kw.cc
@@ -0,0 +1,45 @@
+/*
+ * sf-kw.c: Part of GNU CSSC.
+ * 
+ * 
+ *    Copyright (C) 1997, Free Software Foundation, Inc. 
+ * 
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ * 
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ * 
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * 
+ *
+ * sccs_file::no_id_keywords()
+ */
+
+#ifdef __GNUC__
+#pragma implementation "seqstate.h"
+#endif
+
+#include "cssc.h"
+#include "sccsfile.h"
+
+
+
+void
+sccs_file::no_id_keywords() const 
+{
+  if (flags.no_id_keywords_is_fatal)
+    {
+      quit(-1, "%s: No id keywords.", name);
+    }
+  else
+    {
+      fprintf(stderr, "%s: Warning: No id keywords.\n", name);
+    }
+}
