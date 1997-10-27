2e265ce2cbfc57178ce61cf6a2fe4c5b26a5f72e
James Youngman <jay@gnu.org>
1997-10-27 19:31:23 +0000

    Re-enable some tests I had disables while debugging something else (and had forgotten to re-enable).

--- tests/get/subst.sh
+++ tests/get/subst.sh
@@ -37,24 +37,24 @@ docommand "%${1}%" "egrep \"^_${1}_ \" <${output}" 0 "$2" ""
 
 # Examine each of the things formatted into the file and
 # check them against our expectations.
-#
-# expands_to A "_A_ @(#) keywords.txt 1.1@(#)\n" 
-# expands_to B "_B_ 0\n"                         
-# expands_to C "_C_ 16\n_C_ 17\n"                
-# expands_to E "_E_ 97/10/25\n"                  
-# expands_to F "_F_ s.keywords.txt\n"            
-# expands_to G "_G_ 10/25/97\n"                  
-# expands_to I "_I_ 1.1\n" 
-# expands_to L "_L_ 1\n"                         
-# expands_to M "_M_ keywords.txt\n"	         
-# expands_to P "_P_ ${PWD}/s.keywords.txt\n"     
-# expands_to Q "_Q_ \n"                          
-# expands_to R "_R_ 1\n"                         
-# expands_to S "_S_ 0\n"                         
-# expands_to U "_U_ 13:19:31\n"                  
-# expands_to W "_W_ @(#)keywords.txt\t1.1\n"     
-# expands_to Y "_Y_ \n"                          
-# expands_to Z "_Z_ @(#)\n"                      
+
+expands_to A "_A_ @(#) keywords.txt 1.1@(#)\n" 
+expands_to B "_B_ 0\n"                         
+expands_to C "_C_ 16\n_C_ 17\n"                
+expands_to E "_E_ 97/10/25\n"                  
+expands_to F "_F_ s.keywords.txt\n"            
+expands_to G "_G_ 10/25/97\n"                  
+expands_to I "_I_ 1.1\n" 
+expands_to L "_L_ 1\n"                         
+expands_to M "_M_ keywords.txt\n"	         
+expands_to P "_P_ ${PWD}/s.keywords.txt\n"     
+expands_to Q "_Q_ \n"                          
+expands_to R "_R_ 1\n"                         
+expands_to S "_S_ 0\n"                         
+expands_to U "_U_ 13:19:31\n"                  
+expands_to W "_W_ @(#)keywords.txt\t1.1\n"     
+expands_to Y "_Y_ \n"                          
+expands_to Z "_Z_ @(#)\n"                      
 
 # TODO: better tests for Q, D, H, T, Y
 remove $s $output
@@ -65,7 +65,13 @@ s=s.keys.txt
 remove $s
 uudecode < keys.uue || miscarry could not extract test file.
 docommand K1 "${get} -p -c971025230458 $s" 0 "1.2 1.2\n" "1.2\n1 lines\n"
-docommand K2 "${get} -p -c971025230457 $s" 0 "1.1 1.1\n" "1.1\n1 lines\n"
+docommand K2 "${get} -p -c971025230457 $s" 0 "1.1 1.1\n" \
+	"IGNORE"
+
+# TODO: We currently say Excluded: blah... if a version is 
+# excluded because of the cutoff date.  We should not do that.
+#docommand K2 "${get} -p -c971025230457 $s" 0 "1.1 1.1\n" \
+#	"Excluded:\n1.2\n1.2\n1 lines\n"
 
 
 # tests are finished.
