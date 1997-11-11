8f4f7bc805ba0065f8229d1c5eccecae50dfad3f
James Youngman <jay@gnu.org>
1997-11-11 00:15:14 +0000

    Added config.h.in (which somehow had got lost in the RCS->CVS migration) back in.

--- dummy.cc
+++
@@ -1,9 +0,0 @@
-//
-// This is a dummy function whose sole purpose is to
-// ensure that libsubst.a is not empty.  If you try
-// to link against an empty library on Solaris (2.5.1)
-// you get an error from ld.
-//
-void dummy(void)
-{
-}
