2aefffa68fdec8297132edb2841409aaded93297
James Youngman <jay@gnu.org>
1997-11-30 21:10:26 +0000

    Code split out of sccsfile.h and sccsfile.cc.

---
+++ dtbl-prepend.cc
@@ -0,0 +1,52 @@
+/*
+ * dtbl-prepend.cc: Part of GNU CSSC.
+ * 
+ * 
+ *    Copyright (C) 1997, Free Software Foundation, Inc. 
+ * 
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ * 
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ * 
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * 
+ * CSSC was originally Based on MySC, by Ross Ridge, which was 
+ * placed in the Public Domain.
+ *
+ *
+ * 
+ */
+
+#include "cssc.h"
+#include "sccsfile.h"
+#include "delta-table.h"
+
+#ifdef CONFIG_SCCS_IDS
+static const char rcs_id[] = "CSSC $Id: dtbl-prepend.cc,v 1.1 1997/11/30 21:10:26 james Stab $";
+#endif
+
+
+/* Insert a delta at the start of the delta table. */
+
+void
+cssc_delta_table::prepend(const delta &it)
+{
+  list<struct delta> newlist;
+
+  newlist.add(it);
+  newlist += l;
+
+  l = newlist;
+  
+  update_highest(it);
+}
+
+
---
+++ sccs-delta.cc
@@ -0,0 +1,73 @@
+/*
+ * sf-delta2.c: Part of GNU CSSC.
+ * 
+ * 
+ *    Copyright (C) 1997, Free Software Foundation, Inc. 
+ * 
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ * 
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ * 
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * 
+ * CSSC was originally Based on MySC, by Ross Ridge, which was 
+ * placed in the Public Domain.
+ *
+ *
+ * Members of class delta.
+ *
+ */
+
+#include "cssc.h"
+#include "sccsfile.h"
+#include "delta.h"
+
+#ifdef CONFIG_SCCS_IDS
+static const char rcs_id[] = "CSSC $Id: sccs-delta.cc,v 1.1 1997/11/30 21:10:26 james Exp $";
+#endif
+
+delta &
+delta::operator =(delta const &it)
+{
+  inserted = it.inserted;
+  deleted = it.deleted;
+  unchanged = it.unchanged;
+  type = it.type;
+  id = it.id;
+  date = it.date;
+  user = it.user;
+  seq = it.seq;
+  prev_seq = it.prev_seq;
+  
+  included = it.included;
+  excluded = it.excluded;
+  ignored = it.ignored;
+  have_includes = it.have_includes;
+  have_excludes = it.have_excludes;
+  have_ignores  = it.have_ignores;
+  
+  mrs = it.mrs;
+  comments = it.comments;
+  return *this;
+}
+
+bool delta::removed() const
+{
+  return 'R' == type;
+}
+
+
+
+
+
+/* Local variables: */
+/* mode: c++ */
+/* End: */
