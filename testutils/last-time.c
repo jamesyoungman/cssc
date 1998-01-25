9333d4a8bf36cb1f49aec98fd131a19601225454
James Youngman <jay@gnu.org>
1998-01-25 22:34:57 +0000

    Added last-time.c to the distribution.

--- testutils/last-time.c
+++ testutils/last-time.c
@@ -1,7 +1,34 @@
+/* last-time.c: Part of GNU CSSC.
+ * 
+ *    Copyright (C) 1998, Free Software Foundation, Inc. 
+ * 
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ * 
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ * 
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *
+ * This program is not installed as part of CSSC.  It's just desinged
+ * to help you figure out the year-2038 date at which your C library
+ * gives out.
+ *
+ * No attempt has been made to make this program use the
+ * Autoconf-generated configuration information.
+ */
+
+
+
 #include <stdio.h>
 #include <time.h>
-
-#include "../err_no.h"
 
 
 static int* 
