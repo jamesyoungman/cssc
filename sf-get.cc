05640babbf2bf09d44df9a3f0f3027eda149f8a4
James Youngman <jay@gnu.org>
1998-02-01 18:01:35 +0000

    Added a no_decide flag to sccs_file::get() for the use of delta.  We need to be able to obtain the body in encoded form so that we can feed it to diff(1).

--- sf-get.cc
+++ sf-get.cc
@@ -37,6 +37,7 @@
 #include "delta.h"
 #include "delta-table.h"
 #include "linebuf.h"
+#include "bodyio.h"
 
 // We use @LIBOBJS@ instead now...
 // #ifndef HAVE_STRSTR
@@ -44,7 +45,7 @@
 // #endif
 
 #ifdef CONFIG_SCCS_IDS
-static const char rcs_id[] = "CSSC $Id: sf-get.cc,v 1.14 1998/01/25 22:33:04 james Exp $";
+static const char rcs_id[] = "CSSC $Id: sf-get.cc,v 1.15 1998/02/01 18:01:35 james Exp $";
 #endif
 
 void
@@ -84,122 +85,158 @@ sccs_file::get(mystring gname, class seq_state &state,
 					  struct subst_parms *,
 					  const delta&) const,
 #endif
-	       int show_sid, int show_module, int debug) {
-	ASSERT(mode != CREATE);
+	       int show_sid, int show_module, int debug,
+	       bool no_decode /* = false */)
+{
+  ASSERT(mode != CREATE);
+  bool binary_keyword_warned = false;
+  
 
+  int (*outputfn)(FILE*,const cssc_linebuf*);
+  if (flags.encoded && false == no_decode)
+    outputfn = output_body_line_binary;
+  else
+    outputfn = output_body_line_text;
+	
 
-	seek_to_body();
+  seek_to_body();
 
-	/* The following statement is not correct. */
-	/* "@I 1" should start the body of the SCCS file */
+  /* The following statement is not correct. */
+  /* "@I 1" should start the body of the SCCS file */
 
-	if (read_line() != 'I') {
-		corrupt("Expected '@I'");
+  if (read_line() != 'I') {
+    corrupt("Expected '@I'");
+  }
+  check_arg();
+
+  /* The check on the following line is certainly wrong, since
+   * the first body line need not refer to the first delta.  For
+   * example, SunOS 4.1.1's SCCS implementation doesn't always
+   * start with ^AI 1.
+   */
+  unsigned short first_delta = strict_atous( plinebuf->c_str() + 3);
+  state.start(first_delta, 1); /* 1 means "insert". */
+
+  FILE *out = parms.out;
+
+  while(1) {
+    int c = read_line();
+
+    if (c == -1) { 
+      /* EOF */
+      break;
+    }
+
+    if (c == 0) {
+      /* A non-control line */
+
+      if (debug) {
+	if (state.include_line()) {
+	  putc('I', f);
+	} else {
+	  putc('D', f);
 	}
-	check_arg();
+	putc(' ', f);
+      } else if (!state.include_line()) {
+	continue;
+      }
 
-	/* The check on the following line is certainly wrong, since
-	 * the first body line need not refer to the first delta.  For
-	 * example, SunOS 4.1.1's SCCS implementation doesn't always
-	 * start with ^AI 1.
-	 */
-	unsigned short first_delta = strict_atous( plinebuf->c_str() + 3);
-	state.start(first_delta, 1); /* 1 means "insert". */
+      parms.out_lineno++;
 
-	FILE *out = parms.out;
+      if (show_module)
+	fprintf(out, "%s\t", get_module_name().c_str());
 
-	while(1) {
-		int c = read_line();
-
-		if (c == -1) { 
-			/* EOF */
-			break;
-		}
-
-		if (c == 0) {
-			/* A non-control line */
-
-			if (debug) {
-				if (state.include_line()) {
-					putc('I', f);
-				} else {
-					putc('D', f);
-				}
-				putc(' ', f);
-			} else if (!state.include_line()) {
-				continue;
-			}
-
-			parms.out_lineno++;
-
-			if (show_module)
-			  fprintf(out, "%s\t", get_module_name().c_str());
-
-			if (show_sid)
-			  {
-			    seq_to_sid(state.active_seq()).print(out);
-			    putc('\t', out);
-			  }
-
-			int err;
-			if (subst_fn != NULL) {
-			  // If there is a cutoff date,
-			  // prepare_seqstate() will take account of
-			  // it.  We need the keyword substitution to
-			  // take account of this and substitute the
-			  // correct stuff.... so we figure out what
-			  // delta has actually been selected here...
-
-			  const delta& gotten_delta
-			    = delta_table->delta_at_seq(state.active_seq());
-			   err = (this->*subst_fn)(plinebuf->c_str(), &parms,
-						   gotten_delta);
-			 } else {
-				 err = fputs_failed(fputs(plinebuf->c_str(), out));
-				 if (!parms.found_id 
-				     && check_id_keywords(plinebuf->c_str())) {
-					 parms.found_id = 1;
-				 }
-			 } 
-
-			 if (err || fputc_failed(fputc('\n', out)))
-			   {
-			     quit(errno, "%s: Write error.", gname.c_str());
-			   }
-			 continue;
-		 }
-
-		 /* A control line */
-
-		 check_arg();
-		 seq_no seq = strict_atous(plinebuf->c_str() + 3);
-		if (seq < 1 || seq > highest_delta_seqno()) {
-			corrupt("Invalid serial number");
-		}
-
-		const char *msg = NULL;
-
-		switch(c) {
-		case 'E':
-			msg = state.end(seq);
-			break;
-
-		case 'D':
-		case 'I':
-			msg = state.start(seq, c == 'I');
-			break;
-
-		default:
-			corrupt("Unexpected control line");
-			break;
-		}
-
-		if (msg != NULL) {
-			corrupt(msg);
-		}
+      if (show_sid)
+	{
+	  seq_to_sid(state.active_seq()).print(out);
+	  putc('\t', out);
 	}
 
-	fflush(out);
+      int err;
+      if (subst_fn != NULL)
+	{
+	  // If there is a cutoff date,
+	  // prepare_seqstate() will take account of
+	  // it.  We need the keyword substitution to
+	  // take account of this and substitute the
+	  // correct stuff.... so we figure out what
+	  // delta has actually been selected here...
+
+
+	  // Our keyword substitution function is not
+	  // suitable at the moment for operating
+	  // with an in-memory diestination.  Hence we
+	  // cannot expand keywords for encoded files.
+	  if (flags.encoded)
+	    {
+				// TODO: check against real SCCS and fix me!
+	      if (!binary_keyword_warned)
+		{
+		  fprintf(stderr,
+			  "%s: Warning: Keyword substitution not "
+			  "yet supported for binary files (try the -k flag).\n",
+			  name.c_str());
+		  binary_keyword_warned = true;
+		}
+	      
+	      err = outputfn(out, plinebuf);
+	    }
+	  else
+	    {
+	      // text output, with 
+	      const delta& gotten_delta
+		= delta_table->delta_at_seq(state.active_seq());
+	      err = (this->*subst_fn)(plinebuf->c_str(),
+				      &parms, gotten_delta);
+	      
+	      if (fputc_failed(fputc('\n', out)))
+		err = 1;
+	    }
+	}
+      else
+	{
+	  if (!parms.found_id && plinebuf->check_id_keywords())
+	    parms.found_id = 1;
+	  err = outputfn(out, plinebuf);
+	} 
+
+      if (err)
+	quit(errno, "%s: Write error.", gname.c_str());
+
+      continue;
+    }
+
+    /* A control line */
+
+    check_arg();
+    seq_no seq = strict_atous(plinebuf->c_str() + 3);
+    if (seq < 1 || seq > highest_delta_seqno()) {
+      corrupt("Invalid serial number");
+    }
+
+    const char *msg = NULL;
+
+    switch(c) {
+    case 'E':
+      msg = state.end(seq);
+      break;
+
+    case 'D':
+    case 'I':
+      msg = state.start(seq, c == 'I');
+      break;
+
+    default:
+      corrupt("Unexpected control line");
+      break;
+    }
+
+    if (msg != NULL) {
+      corrupt(msg);
+    }
+  }
+
+  fflush(out);
 }
 
 /* Local variables: */
