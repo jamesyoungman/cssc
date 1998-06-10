7825ba9622d7fa9979e7bf8bd4833bc3797f772f
James Youngman <jay@gnu.org>
1998-06-10 21:13:02 +0000

    Ensure the test suite is not run by root (because that makes "test -w" return misleading results).

--- tests/bsd-sccs/driver-basic.sh
+++ tests/bsd-sccs/driver-basic.sh
@@ -5,6 +5,13 @@
 # Import common functions & definitions.
 . ../common/test-common
 
+# The test suite fails if you run it as root, particularly because
+# "test -w foo" returns 0 if you are root, even if foo is a readonly
+# file.
+# So please don't run the test suite as root, because it will spuriously
+# fail.
+true 
+. ../common/not-root
 
 
 # Find the sccs driver program.   It's not done in 
