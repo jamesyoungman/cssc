e9a63e74a9f429a9ca78e640105d4005893c75be
James Youngman <jay@gnu.org>
1998-09-03 22:25:23 +0000

    Added test that check for correct behabiour when the s-file is absent.

---
+++ tests/prt/exists.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+# exists.sh:  What if the input file doesn't exist?
+
+# Import common functions & definitions.
+. ../common/test-common
+
+s=s.testfile
+
+remove $s
+docommand e1 "${prt} $s" 1 "IGNORE" "IGNORE"
+remove $s
+
+success
