f645500cf3beadcd8c3ca8b5c35a21a3bd660a88
James Youngman <jay@gnu.org>
1999-03-14 13:55:36 +0000

    Check that the "initial number of lines" is correct for admin -i.

--- tests/admin/i-option.sh
+++ tests/admin/i-option.sh
@@ -5,45 +5,82 @@
 # Import common functions & definitions.
 . ../common/test-common
 
+g=bar
+s=s.$g
+remove $s foo $g
 
-remove s.bar foo bar
-
-remove foo
+remove $g
 echo '%M%' > foo
-test `cat foo` = '%M%' || miscarry cannot create file foo.
+test x`cat foo` = x'%M%' || miscarry cannot create file foo.
 
-docommand I1 "${admin} -ifoo s.bar" 0 "" IGNORE
-docommand I2 "${get} -r1.1 -p s.bar"      0 "bar\n" IGNORE
+docommand I1 "${admin} -ifoo $s" 0 "" IGNORE
+docommand I2 "${get} -r1.1 -p $s"      0 "$g\n" IGNORE
 remove foo s.bar
 
 # -i on its own means read from stdin.
 
 echo baz | \
-docommand I3 "${admin} -i s.bar" 0 "" IGNORE
-docommand I4 "${get} -r1.1 -p s.bar"      0 "baz\n" IGNORE
+docommand I3 "${admin} -i $s" 0 "" IGNORE
+docommand I4 "${get} -r1.1 -p $s"      0 "baz\n" IGNORE
 
+
+remove $s $g foo
 
 # If the file specified by -i does not exist, make sure that not
 # only is there a fatal exit, but neither the s-file or the x-file is 
 # left behind.
-remove foo s.foo x.foo 
-docommand I5 "${admin} -ifoo s.foo" 1 "" IGNORE
+
+g=foo
+s=s.$g
+x=x.$g
+
+remove $g $s $x
+docommand I5 "${admin} -i$g $s" 1 "" IGNORE
 
 echo_nonl "I6..."
-if test -f s.foo; then
-    fail I6: The file s.foo should not have been created.
+if test -f $s; then
+    fail I6: The file $s should not have been created.
 fi
 echo 'passed'
 
 echo_nonl "I7..."
-if test -f x.foo; then
-    fail I7: The temporary file x.foo should have been deleted.
+if test -f $x; then
+    fail I7: The temporary file $x should have been deleted.
 fi
-remove foo s.foo x.foo 
+remove $g $s $x
 echo 'passed'
 
 
 
-remove s.bar foo bar command.log 
+# Now check that we get the number of inserted lines correct for the
+# first delta.
+
+remove $g $s $x
+echo_nonl "" > $g
+docommand I8 "${admin} -i$g $s" 0 "" IGNORE
+docommand I9 "${prs}  -d:Li: $s" 0 "00000\n" IGNORE
+
+# Check that the deleted and unchanged lines are also zero.
+docommand I10 "${prs} -d:Ld: $s" 0 "00000\n" IGNORE
+docommand I11 "${prs} -d:Lu: $s" 0 "00000\n" IGNORE
+
+remove $g $s $x
+echo_nonl "\n" > $g
+docommand I12 "${admin} -i$g $s" 0 "" IGNORE
+docommand I13 "${prs}  -d:Li: $s" 0 "00001\n" IGNORE
+
+# Check that the deleted and unchanged lines are also zero.
+docommand I14 "${prs} -d:Ld: $s" 0 "00000\n" IGNORE
+docommand I15 "${prs} -d:Lu: $s" 0 "00000\n" IGNORE
+
+
+remove $g $s $x
+echo_nonl "hello\nworld\n" > $g
+docommand I16 "${admin} -i$g $s" 0 "" IGNORE
+docommand I17 "${prs}  -d:Li: $s" 0 "00002\n" IGNORE
+
+
+
+remove $x s.bar s.foo foo bar command.log 
 success
 
