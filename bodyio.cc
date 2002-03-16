ebece52ec30a5af2f90aa2a9991d2b6c8a28e2c9
James Youngman <jay@gnu.org>
2002-03-16 22:02:13 +0000

    valgrind-detected bug: do keyword checking on the uuencoded version of the data

--- bodyio.cc
+++ bodyio.cc
@@ -231,7 +231,7 @@ body_insert_binary(const char iname[], const char oname[],
 	  // For some odd reason, SCCS seems to check
 	  // the encoded form for ID keywords!  We know
 	  // that strlen() on the UUENCODEd data is safe.
-	  if (check_id_keywords(inbuf, strlen(inbuf)))
+	  if (check_id_keywords(outbuf, strlen(outbuf)))	// XXX used to check inbuf
 	    *idkw = kw = true;
 	}
       
