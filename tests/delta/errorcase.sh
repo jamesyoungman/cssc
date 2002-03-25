ed7dbcd1ceaa36ef1312aa43170f29f51ac9c92b
James Youngman <jay@gnu.org>
2002-03-25 23:58:57 +0000

    Extra test cases for the authorised user list.

--- tests/delta/errorcase.sh
+++ tests/delta/errorcase.sh
@@ -11,7 +11,8 @@ g=foo
 s=s.$g
 p=p.$g
 z=z.$g
-files="$g $s $p $z ${g}_1 ${g}_2 $g"
+q=q.$g
+files="$g $s $p $z ${g}_1 ${g}_2 $g $q"
 
 remove $files
 
@@ -24,6 +25,8 @@ append() {
 
 if wrong_group=`../../testutils/user foreigngroup`
 then
+    true
+else
     miscarry "could not select the name of a group to which you do not belong"
 fi
 # echo "You do not belong to group number" $wrong_group
@@ -36,14 +39,63 @@ append $g "test data"
 docommand E3 "${delta} -yNoComment $s"   0 IGNORE IGNORE 
 
 # Now set up the authorised groups list.   
-docommand E4 "${get} -e $s"   0 IGNORE IGNORE 
+docommand E4 "${admin} -a${wrong_group} $s" 0 IGNORE IGNORE
+
+# cannot do get -e if you are not in the authorised user list.
+docommand E5 "${get} -e $s"   1 IGNORE IGNORE 
+
+# Momentarily zap the authorised user list so that "get -e" works.
+docommand E6 "${admin} -e${wrong_group} $s" 0 IGNORE IGNORE
+docommand E7 "${get} -e $s"   0 IGNORE IGNORE
+docommand E8 "${admin} -a${wrong_group} $s" 0 IGNORE IGNORE
+
 append $g "more test data"
-docommand E5 "${admin} -a${wrong_group} $s" 0 IGNORE IGNORE
-docommand E6 "${delta} -yNoComment $s"   1 IGNORE IGNORE 
+
+# delta should still fail if we are not in the authorised user list
+# (in other words the list is checked both by get -e and delta).
+docommand E9 "${delta} -yNoComment $s"   1 IGNORE IGNORE
 
 # Remove the authorised group list; check-in should now work
-docommand E7 "${admin} -e${wrong_group} $s" 0 IGNORE IGNORE
-docommand E8 "${delta} -yNoComment $s"   0 IGNORE IGNORE 
+docommand E10 "${admin} -e${wrong_group} $s" 0 IGNORE IGNORE
+docommand E11 "${delta} -yNoComment $s"   0 IGNORE IGNORE 
+
+
+# Now, what if the authorised user list just excludes?
+remove $s
+if mygroup=`../../testutils/user group`
+then
+    true
+else
+    miscarry "could not determine group-id"
+fi
+
+if myname=`../../testutils/user name`
+then
+    true
+else
+    miscarry "could not determine user name"
+fi
+
+# Regular SCCS does not underatand the use of "!username" 
+# to specifically exclude users.  Hence for compatibility 
+# nor must we.
+docommand E12 "${admin} -n $s"              0 IGNORE IGNORE 
+docommand E13 "${admin} -a${mygroup} $s"    0 IGNORE IGNORE
+docommand E14 "${admin} -a\!${myname} $s"   0 IGNORE IGNORE
+docommand E15 "${get} -e $s"                0 IGNORE IGNORE
+# this means that the above tests should succeed.
+
+# Check - use of delta when a q-file already exists...
+touch $q
+test -r $q || miscarry "could not create file $q"
+docommand E16 "${delta} -yNoComment $s" 1 IGNORE IGNORE
+remove $q
+
+# Unreadable g-file should also cause a failure. 
+chmod 0 $g
+docommand E17 "${delta} -yNoComment $s" 1 IGNORE IGNORE
+chmod +r $g
+docommand E18 "${delta} -yNoComment $s" 0 IGNORE IGNORE
 
 
 remove $files 
