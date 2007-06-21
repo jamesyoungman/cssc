42e63a3a8ac87cfa7452292e365f086da1ca86ca
James Youngman <jay@gnu.org>
2007-06-21 00:13:55 +0000

    Updated copyright notice in the sccs.c file to be consistent with the COPYING.bsd file

--- bsd/sccs.c
+++ bsd/sccs.c
@@ -13,11 +13,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by the University of
- *      California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -44,7 +40,7 @@ static const char copyright[] =
 "@(#) Copyright (c) 1998\n"
 "Free Software Foundation, Inc.  All rights reserved.\n";
 #endif /* not lint */
-static const char filever[] = "$Id: sccs.c,v 1.42 2007/06/20 09:54:23 james_youngman Exp $";
+static const char filever[] = "$Id: sccs.c,v 1.43 2007/06/21 00:13:55 james_youngman Exp $";
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
