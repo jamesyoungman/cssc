922205ff223d96664aeb6eee1a885ef728eabb5c
James Youngman <jay@gnu.org>
2010-04-19 01:15:35 +0100

    Use the standard string class.
    * mystring.h: Assume the availability of the <string> header, so
    that mystring is always a typedef for std::string.
    * mystring.cc: Delete.
    * Makefile.am (sname_srcs): Delete mystring.cc.
    
    Signed-off-by: James Youngman <jay@gnu.org>

--- mystring.h
+++ mystring.h
@@ -2,7 +2,7 @@
  * mystring.h: Part of GNU CSSC.
  * 
  * 
- *    Copyright (C) 1997,1998,1999,2001,2007 Free Software Foundation, Inc. 
+ *    Copyright (C) 1997,1998,1999,2001,2007,2010 Free Software Foundation, Inc. 
  * 
  *    This program is free software: you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by
@@ -20,122 +20,16 @@
  * CSSC was originally Based on MySC, by Ross Ridge, which was 
  * placed in the Public Domain.
  *
- *
  * Defines the class mystring.
  */
 
 #ifndef CSSC__MYSTRING_H__
 #define CSSC__MYSTRING_H__
 
-// If we have the header file <string>, then
-// use that rather than mystring.
-#ifdef HAVE_STRING
-#define USE_STANDARD_STRING
-#endif
-
-
-#ifdef USE_STANDARD_STRING
-
 #include <string>
 typedef std::string mystring;
 
-
-#else /* Use our own "mystring". */
-
-#ifdef __GNUC__
-#pragma interface
-#endif
-
-#define STR_OFFSET (offsetof(ptr, str))
-#define STR_PTR(str) ((ptr *)((str) - STR_OFFSET))
-
-/* This is a very simple string class.  Its purpose is mainly to
-   simplify the allocation of strings. */
-
-
-class mystring
-{
-  class MyStrRep;               // nested class.
-  class ModifiableReference;
-  friend class MyStrRep;                // nested class.
-  friend class ModifiableReference;
-  
-  MyStrRep *rep;
-  
-  
-public:
-  typedef unsigned long size_type;
-  typedef char charT;
-  
-  static const size_type npos = static_cast<size_type>(-1);
-  
-  
-  // Constructors...
-  mystring();
-  mystring(const charT*);
-  mystring(const charT*, size_type); // first N characters.
-  mystring(const mystring&);
-
-  // Assignment
-  mystring& operator=(const mystring&);
-  mystring& assign(const mystring&, size_type pos);
-  
-  // Observers
-  bool valid() const;
-  bool empty() const;
-  size_type length() const;
-  
-  // Data access
-
-  // IRIX's CC seems to be unable to cope with functions disambiguated only 
-  // by constness.   It says "error(3392): cannot overload functions 
-  // distinguished by return type alone".   It's remotely possible that 
-  // some Autoconf black magic has perpetrated #define const /*nothing*/
-  // and that's what's causing the problem.  If so, I shall be cross...:-)
-  charT at(size_type) const;
-  ModifiableReference at(size_type);
-  
-  
-  // Casts etc.
-  const charT *c_str() const;
-  
-  // Slicing...
-  mystring substr(size_type first, size_type last) const;
-
-  // Operators...
-  mystring operator+(const mystring&) const;
-  bool operator==(const mystring&) const;
-  bool operator!=(const mystring&) const;
-
-
-  // Searching..
-  size_type find_last_of(charT) const;
-
-
-private:
-
-  class ModifiableReference
-  {
-    mystring &s;
-    size_type pos;
-  public:
-    ModifiableReference(mystring& sref, size_type i) : s(sref), pos(i) { }
-    charT& operator=(charT ch);
-    operator char() const;
-  };
-
-  int compare(const mystring &) const;
-
-  // Copy management.
-  void prepare_for_writing();
-};
-
-
-#endif /* USE_STANDARD_STRING */
-
-
 #endif /* __MYSTRING_H__ */
-
 /* Local variables: */
 /* mode: c++ */
 /* End: */
