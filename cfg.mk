e2426d63eb7589039d8780973c9db1beaddb4b60
James Youngman <jay@gnu.org>
2010-05-02 12:57:18 +0100

    Use $(FOO) in Makefile.am rather than @FOO@.
    * tests/Makefile.am: Automake defines $(VALGRIND) from @VALGRIND@
    automatically anyway.
    * cfg.mk (_makefile_at_at_check_exceptions): Exempt a couple of
    usages by the Google C++ Testing Framework.
    * Makefile.am: Use $(LIBOBJS) instead of @LIBOBJS@.
    * unit-tests/Makefile.am (AM_LIBS): Likewise for $(PTHREAD_LIBS).
    (AM_CXXFLAGS): Likewise for  $(PTHREAD_CFLAGS).
    
    Signed-off-by: James Youngman <jay@gnu.org>

--- cfg.mk
+++ cfg.mk
@@ -3,3 +3,8 @@
 # d41d8cd98f00b204e9800998ecf8427e is the md5sum of an empty input.
 # We use that initially to silence the error we would otherwise get.
 old_NEWS_hash = d41d8cd98f00b204e9800998ecf8427e
+
+
+# Exempt the Google C++ Testing Framework from the @FOO@ syntax-check
+# on order not to have to maintain local diffs.
+_makefile_at_at_check_exceptions = ' && !/PTHREAD_CFLAGS/ && !/PTHREAD_LIBS/'
