a8a798bd3b9bf8061e3a34114ef43c4e6121247d
James Youngman <jay@gnu.org>
2010-05-02 12:43:12 +0100

    Silence a spurious error about changing old NEWS entries.
    * cfg.mk (old_NEWS_hash): Set old_NEWS_hash to the md5sum of a
    zero-length input in order to silence a spurious error about
    changing old NEWS entries.
    
    Signed-off-by: James Youngman <jay@gnu.org>

---
+++ cfg.mk
@@ -0,0 +1,5 @@
+# The NEWS version check does not (yet) work for us because of
+# differences in the format of the file, but
+# d41d8cd98f00b204e9800998ecf8427e is the md5sum of an empty input.
+# We use that initially to silence the error we would otherwise get.
+old_NEWS_hash = d41d8cd98f00b204e9800998ecf8427e
