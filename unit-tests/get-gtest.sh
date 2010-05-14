f065830d61d2690fb7eec1a9ab0ded5288b97840
James Youngman <jay@gnu.org>
2010-05-14 10:11:59 +0100

    Configure and build googletest in our own configure/Makefile.
    * unit-tests/Makefile.am: Build the Google C++ Testing Framework
    code in this Makefile rather than forcing it to have its own
    configure.
    * bootstrap.conf (bootstrap_epilogue): googletest now has no
    separate configure step, so no need to bootstrap in that directory
    either.
    * unit-tests/get-gtest.sh: Script for checking out the google unit
    test code.

---
+++ unit-tests/get-gtest.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+# Checks out a copy of the Google C++ Testing Framework.
+url='http://googletest.googlecode.com/svn/trunk/'
+module='googletest'
+
+usage() {
+    echo "usage: $0 svn-version"
+}
+
+main() {
+    if [ -z "$module" ]; then
+	echo "Please specify which directory to put the googletest code in (by editing $0)" >&2
+	usage >&2
+	exit 1
+    fi
+    
+    if [ -d "$module" ] && [ -d "$module"/.svn ]; then
+	( cd "$module" && svn update -r "$1" ) || return 1
+    else
+	svn checkout "$url"@"$1" "$module" || return 1
+    fi
+}
+
+main "$@"
