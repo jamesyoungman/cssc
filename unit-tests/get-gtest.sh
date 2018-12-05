bacbef1405f3f5c1e52c95f7c0052bcada305250
Martin Wilck <mwilck@suse.com>
2018-12-05 11:55:58 +0100

    get-gtest.sh: googletest moved to git repo
    googletest isn't maintained in svn any more.

--- unit-tests/get-gtest.sh
+++ unit-tests/get-gtest.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
-# Checks out a copy of the Google C++ Testing Framework.
-url='http://googletest.googlecode.com/svn/trunk/'
-module='googletest'
+# This should match git submodule configuration
+module=../googletest
 
 usage() {
     echo "usage: $0 svn-version"
@@ -13,12 +12,38 @@ main() {
 	usage >&2
 	exit 1
     fi
-
-    if [ -d "$module" ] && [ -d "$module"/.svn ]; then
-	( cd "$module" && svn update -r "$1" ) || return 1
-    else
-	svn --non-interactive checkout "$url"@"$1" "$module" || return 1
+    if [ -z "$1" ]; then
+	echo "Please specify a commit or tag from $module" >&2
+	usage >&2
+	exit 1
     fi
+
+    if [ ! -d "$module" ] || [ ! -e "$module"/.git ] || \
+	   [ ! -d "$module"/googletest ]; then
+	echo "error: $module is not a googletest git repo" >&2
+	exit 1
+    fi
+    head=$(git -C "$module" log -n 1 --pretty=%H HEAD)
+    [ -n "$head" ] || {
+	echo "error getting hash of HEAD in $module" >&2
+	exit 1
+    }
+    trgt=$(git -C "$module" log -n 1 --pretty=%H "$1")
+    [ -n "$trgt" ] || {
+	echo "error: is $1 is $1 a valid tag or commit ID in $module?" >&2
+	exit 1
+    }
+    if [ "$head" != "$trgt" ]; then
+	git -C "$module" checkout $1 || {
+	    echo "error checking out $1 in $module" >&2
+	    usage >&2
+	    exit 1
+	}
+	git add "$module"
+	git commit -m "Checked out googletest $1"
+    fi
+    ln -sf "$module"/googletest .
 }
 
+git submodule update
 main "$@"
