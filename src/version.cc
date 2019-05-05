eb544a6038281d51f9e06b06309674bc4065deca
James Youngman <jay@gnu.org>
2019-05-05 21:14:07 +0100

    Include config.h before other files.
    * build-aux/find-out-of-order-includes.sh: Script for identifying
    source files which include other headers before "config.h", which
    gnulib doesn't like.
    * src/version.cc: Include "config.h" before <stdio.h> to avoid
    complaints from gnulib.

---
+++ src/version.cc
@@ -0,0 +1,9 @@
+#include "config.h"
+#include <stdio.h>
+#include "version.h"
+
+#define version_core "GNU CSSC 1.4.1-rc1"
+
+extern const char cssc_version_rcs[] = "$Version: " version_core " $";
+extern const char cssc_version_sccs[] = "@(#) " version_core;
+extern const char cssc_version[] = version_core;
