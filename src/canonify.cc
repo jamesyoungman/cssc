f0c5adca5d42492a7028441f332d7e9e444c2b0f
James Youngman <jay@gnu.org>
2019-05-28 13:12:26 +0100

    Use gnulib's canonicalize module.
    * bootstrap.conf (modules): add canonicalize.
    * src/canonify.cc (canonify_filename): remove implementation replace
    with call into gnulib.
    * src/Makefile.am (LDADD): link against gnulib.  Remove unuseful LDADD
    overrides.

--- src/canonify.cc
+++ src/canonify.cc
@@ -26,108 +26,31 @@
 #include <limits>
 #include <string>
 #include <vector>
+#include <stdlib.h>		// free
 
 #include "sccsname.h"
 #include "failure.h"
-#include "file.h"
+#include "failure_or.h"
 
 
 #include <cstring>
 
-#include <unistd.h>		// chdir()
-#include <string.h>		// strerror
-#include <stddef.h>
+#include "canonicalize.h"
 
 using std::string;
-
-namespace
-{
-static
-cssc::FailureOr<string::size_type>
-new_cap(string::size_type current)
-{
-  string::size_type increment = current/2;
-  if (0 == increment)
-    increment = 1;
-  if (std::numeric_limits<string::size_type>::max() - increment < current)
-    return cssc::make_failure_from_errno(ERANGE);
-
-  return current + increment;
-}
-}  // unnamed namespace
-
-static cssc::FailureOr<string>
-get_current_directory()
-{
-  std::vector<char> result;
-  result.reserve(16);
-  for (;;)
-    {
-      result.resize(result.capacity());
-      const char *p = getcwd(result.data(), result.capacity());
-      if (nullptr != p)
-	{
-	  return string(p);	// success!
-	}
-
-      if (errno != ERANGE)
-	return cssc::make_failure_from_errno(errno);
-
-      // The buffer was too small, increase it.
-      auto updated = new_cap(result.capacity());
-      if (!updated.ok())
-	return updated.fail();	// Failed.
-      // Ensure we make progress at every iteration.
-      ASSERT(*updated > result.capacity());
-      result.reserve(*updated);
-    }
-}
-
 
 cssc::FailureOr<string>
 canonify_filename(const char* fname)
 {
-  string dirname, basename;
-  split_filename(fname, dirname, basename);
-  bool changed_dir = false;
-
-  cssc::FailureOr<string> old = get_current_directory();
-  if (!old.ok())
-    return old;			// failure
-  const string old_dir(*old);
-
-  if (dirname != "")
+  char *p = canonicalize_filename_mode(fname, CAN_EXISTING);
+  if (nullptr == p)
     {
-      if (0 != chdir(dirname.c_str()))
-	{
-	  return cssc::make_failure_builder_from_errno(errno)
-	    << "unable to chdir to " << dirname;
-	}
-      changed_dir = true;
+      return cssc::make_failure_from_errno(errno);
     }
-
-  // Now that we have changed working directory, all return paths need
-  // to restore the working directory.
-  ResourceCleanup recover_cwd([old_dir, changed_dir](){
-      if (!changed_dir)
-	return;
-      if (0 != chdir(old_dir.c_str()))
-	{
-	  fatal_quit(2, "unable to restore current directory to %s: %s",
-		     old_dir.c_str(), strerror(errno));
-	}
-    });
-
-  cssc::FailureOr<string> curr = get_current_directory();
-  if (!curr.ok())
-    return curr;		// failure
-
-  string canonical_dir(*curr);
-  return string(canonical_dir + string("/") + basename);
+  std::string result(p);
+  free(p);
+  return result;
 }
-
-
-
 
 /* Local variables: */
 /* mode: c++ */
