2b10941b53317b968fb15bb872e3f5fa63f13a5b
James Youngman <james@youngman.org>
2026-07-04 23:10:04 +0100

    tests: fix broken quoting.
    * tests/get/defsid.sh: fix broken quoting "A"B"C" here, B is not
    actually quoted. In this case it didn't need it anyway, so no harm was
    done.  But better to fix the quoting anyway.

--- tests/get/defsid.sh
+++ tests/get/defsid.sh
@@ -62,12 +62,12 @@ docommand e1 "${admin} -n $s" 0 IGNORE IGNORE
 docommand e2 "${admin} -fd100 $s" 0 IGNORE IGNORE
 docommand e3 "${vg_get} -e $s" 0 "1.1\nnew delta 100.1\n0 lines\n" ""
 docommand e4 "echo "hello" >> $g" 0 "" ""
-docommand e5 "${delta} -y"NoComment" $s" 0 IGNORE IGNORE
+docommand e5 "${delta} -yNoComment $s" 0 IGNORE IGNORE
 # prs $s
 
 docommand e6 "${vg_get} -e $s" 0 "100.1\nnew delta 100.2\n1 lines\n" ""
-docommand e7 "echo "there" >> $g" 0 "" ""
-docommand e8 "${delta} -y"NoComment" $s" 0 IGNORE IGNORE
+docommand e7 "echo there >> $g" 0 "" ""
+docommand e8 "${delta} -yNoComment $s" 0 IGNORE IGNORE
 # prs $s
 
 
