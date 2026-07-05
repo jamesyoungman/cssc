03c636594a8bfe45b220bab82e3f7982d3297103
James Youngman <james@youngman.org>
2026-07-05 09:19:38 +0100

    tests: improve style and consistency of tests/binary/auto.sh
    * tests/binary/auto.sh: improve quoting, fix indentation, eliminate
    unused variables and functions.

--- tests/binary/auto.sh
+++ tests/binary/auto.sh
@@ -6,48 +6,29 @@
 . ../common/real-thing.sh
 . ../common/config-data.sh
 
-
-
-good() {
-    if $expect_fail ; then
-        echo UNEXPECTED PASS
-    else
-        echo passed
-    fi
-}
-
-bad() {
-    if $expect_fail ; then
-        echo failed -- but we expected that.
-    else
-        fail "$@"
-    fi
-}
-
-
 # test_bin:
 # Usage:   test_bin LABEL <contents>
 #
 # create a file containing the specified argument and check
 # that it is encoded as a binary file.
 test_bin() {
-set_and_maybe_print_step_label_with_dots "${1}"
-shift
+    set_and_maybe_print_step_label_with_dots "${1}"
+    shift
 
-rm -f infile $s
-echo_nonl "$@" > infile
-if ${vg_admin} -iinfile ${adminflags} $s >/dev/null 2>&1
-then
-    if ( ${prs} -d:FL: $s 2>/dev/null; echo foo ) | grep encoded >/dev/null 2>&1
+    rm -f -- infile "${s}"
+    echo_nonl "$@" > infile
+    if ${vg_admin} -iinfile ${adminflags} $s >/dev/null 2>&1
     then
-        good
+	if ( ${prs} -d:FL: "${s}" 2>/dev/null; echo foo ) | grep encoded >/dev/null 2>&1
+	then
+            echo passed
+	else
+            fail "${label}" "input did not produce an encoded s-file."
+	fi
     else
-        bad $label input did not produce an encoded s-file.
+	fail "${label}" "${vg_admin} returned exit status ${?}."
     fi
-else
-    bad $label ${vg_admin} returned exit status $?.
-fi
-rm -f infile $s
+    rm -f -- infile "${s}"
 }
 
 # test_ascii:
@@ -55,66 +36,66 @@ rm -f infile $s
 # As for test_bin, but the resulting SCCS file must NOT be encoded.
 #
 test_ascii() {
-set_and_maybe_print_step_label_with_dots "${1}"
-shift
+    set_and_maybe_print_step_label_with_dots "${1}"
+    shift
 
-rm -f infile $s
-echo_nonl "$@" > infile
-if ${vg_admin} -iinfile ${adminflags} $s >/dev/null 2>&1
-then
-    if ( ${prt} -f $s 2>/dev/null ; echo foo ) | grep encoded >/dev/null 2>&1
+    rm -f infile "${s}"
+    echo_nonl "$@" > infile
+    if ${vg_admin} -iinfile ${adminflags} "${s}" >/dev/null 2>&1
     then
-        bad $label input produced an encoded s-file and should not have.
+	if ( ${prt} -f $s 2>/dev/null ; echo foo ) | grep encoded >/dev/null 2>&1
+	then
+            fail "${label}" "input produced an encoded s-file and should not have."
+	else
+            echo passed
+	fi
     else
-        good
+	fail "${label}" "${vg_admin} returned exit status ${?}."
     fi
-else
-    bad $label ${vg_admin} returned exit status $?.
-fi
-rm -f infile $s
+    rm -f -- infile "${s}"
 }
 
 g=testfile
-s=s.$g
-z=z.$g
-x=z.$g
-p=p.$g
-files="$s $z $x $p"
+s="s.${g}"
+z="z.${g}"
+x="z.${g}"
+p="p.${g}"
 
-remove command.log log  base [sxzp].$g
+remove command.log log  base "${g}" "${s}" "${z}" "${x}" "${p}"
 
-expect_fail=false
 adminflags=""
+
+if test -z "${binary_support}"
+then
+    abandon_test_script "Variable binary_support should have been set to either true or false by config-data.sh."
+fi
 
 if $binary_support
 then
+    test_ascii a1 "foo\n"
+    test_ascii a2 "foo\nbar\n"
+    test_ascii a3 ""                        # an empty input file should be OK.
+    test_bin   b4 "\001\n"                  # line starts with control-a
+    test_ascii a5 "x\001\n"                 # line contains ^A but doesn't start with it.
 
-test_ascii a1 "foo\n"
-test_ascii a2 "foo\nbar\n"
-test_ascii a3 ""                        # an empty input file should be OK.
-test_bin   b4 "\001\n"                  # line starts with control-a
-test_ascii a5 "x\001\n"                 # line contains ^A but doesn't start with it.
+    adminflags=-b
+    test_bin   b6 "foo\n"                   # Test manual encoding override.
+    test_bin   b7 "x\000y\n"                # ASCII NUL.
+    test_bin   b8 "\000"                    # Just the ASCII NUL alone.
+    test_bin   b9 "\001"                    # Just the ^A alone.
+    adminflags=
 
-adminflags=-b
-test_bin   b6 "foo\n"                   # Test manual encoding override.
-test_bin   b7 "x\000y\n"                # ASCII NUL.
-test_bin   b8 "\000"                    # Just the ASCII NUL alone.
-test_bin   b9 "\001"                    # Just the ^A alone.
-adminflags=
-
-if $TESTING_CSSC
-then
-    ## Real SCCS fails on these inputs:-
-    test_bin   fb10 "foo"               # no newline at end of file.
-    test_ascii fa11 "x\000y\n"          # ASCII NUL.
+    if $TESTING_CSSC
+    then
+	## Real SCCS fails on these inputs:-
+	test_bin   fb10 "foo"               # no newline at end of file.
+	test_ascii fa11 "x\000y\n"          # ASCII NUL.
+    else
+	echo "Some tests skipped (since SCCS fails them but CSSC should pass)"
+    fi
 else
-    echo "Some tests skipped (since SCCS fails them but CSSC should pass)"
-fi
-
-remove command.log $s $p $g $z $x infile
-
-else
-echo "No binary file support -- tests skipped"
+    echo "No binary file support -- tests skipped"
 fi # binary support.
 
+remove command.log "${s}" "${p}" "${g}" "${z}" "${x}" infile
 success
