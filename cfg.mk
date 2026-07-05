0b0ab9e9d566b9e64ee1094dcbd5d2e34c6d6d16
James Youngman <james@youngman.org>
2026-07-05 22:26:00 +0100

    maint: post-release administrivia
    * NEWS: Add header line for next release.
    * .prev-version: Record previous version.
    * cfg.mk (old_NEWS_hash): Auto-update.

--- cfg.mk
+++ cfg.mk
@@ -2,7 +2,7 @@
 # differences in the format of the file, but
 # d41d8cd98f00b204e9800998ecf8427e is the md5sum of an empty input.
 # We use that initially to silence the error we would otherwise get.
-old_NEWS_hash = c09c856305b563e79c1bbc913d7fbb67
+old_NEWS_hash = 9bd58f124b49f7e69e6e4f3916f07ab1
 
 
 # Exempt the Google C++ Testing Framework from the syntax-check which
