4c3b6a8c75bab2e599d06b6fcc26c7949d842995
James Youngman <james@youngman.org>
2026-07-05 13:14:16 +0100

    maint: build-aux/shell_check.sh: don't miss out most files.
    * build-aux/shell_check.sh: fix bug in awk script which had meant that
    most files were not checked.

--- build-aux/shell_check.sh
+++ build-aux/shell_check.sh
@@ -1,5 +1,18 @@
 #! /bin/sh
 
+shellcheck_postprocess() {
+    sed -e 's/: note:/: warning:/'
+}
+
+run_shellcheck() {
+    # use fd 3 as a place to put  shellcheck's stderr output.
+    # send its stderr into a pipe where it can be modified.
+    # then reverse, this, so our stdout shows shellcheck's stdout
+    # and our stderr shows shellcheck's modified stderr.
+    shellcheck "$@" | shellcheck_postprocess
+}
+
+
 awk -e '
 {
     dir=$1;
@@ -23,7 +36,7 @@ END {
 	    then
 		# We specifically want to word split ${paths} here.
 		# shellcheck disable=SC2086
-		shellcheck -f gcc -x -e SC2121,SC2006,SC2119,SC2012,SC2196,SC2268,SC2003 ${paths}  || exit 1
+		run_shellcheck -f gcc -x -e SC2121,SC2006,SC2119,SC2012,SC2196,SC2268,SC2003 ${paths} || exit 1
 	    fi
         ) || outer_rv=$?
     done
